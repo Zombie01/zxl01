d78613987a2cf1dc3b5a67de92f015b525a69038
apondon <apondon@126.com>
2020-04-21 09:25:59 +0800

    后端项目初始化

---
+++ chileme-sev/config.js
@@ -0,0 +1,4 @@
+module.exports ={
+    port:3000,   // 服务器端口
+    path:'mongodb://localhost:27017/chileme181'      // 数据库地址
+}
---
+++ chileme-sev/utils/util.js
@@ -0,0 +1,19 @@
+// 1.时间戳转换成 yyyy-mm-dd hh:mm:ss  格式的方法
+const transTime = num =>{
+
+}
+// 2.生成随机颜色的方法
+const randomColor = () =>{
+
+}
+// 3.将状态码转换成汉字的方法 
+const transCode = num =>{
+
+}
+
+
+module.exports={
+    transTime,
+    randomColor,
+    transCode
+}
