17aa9b660aacfc04a7137f0cb32ea0c0d1fe3c39
apondon <apondon@126.com>
2020-04-21 09:34:23 +0800

    修改路由按钮显示bug

--- chileme-cli/src/App.vue
+++ chileme-cli/src/App.vue
@@ -1,8 +1,8 @@
 <template lang='pug'>
   div(id="app")
     div(id="nav")
-      router-link (to="/") Home |
-      router-link (to="/about") About
+      router-link(to="/") Home |
+      router-link(to="/about") About
     
     router-view
   
