f562dc888eb3355fb11fe1f181eee17ae2841fb7
apondon <apondon@126.com>
2020-04-22 11:17:11 +0800

    安装配置elementui

--- chileme-cli/src/main.js
+++ chileme-cli/src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Axios from 'axios' // 引入axios
-
+// 引入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 
 Vue.prototype.Axios = Axios  // 配置axios
-
+Vue.use(ElementUI) //配置elementUI
 new Vue({
   router,
   store,
