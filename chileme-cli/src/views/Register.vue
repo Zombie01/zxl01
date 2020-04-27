2c5e55deb76da2bef96214c832cd1fd33076858e
apondon <apondon@126.com>
2020-04-27 11:25:59 +0800

    调用接口报错

--- chileme-cli/src/views/Register.vue
+++ chileme-cli/src/views/Register.vue
@@ -6,21 +6,32 @@
                 el-form-item(
                     size='medium',
                     label="手机号",
-                    prop="mobeli",
+                    prop="mobile",
                     :rules="[{ required: true, message: '手机号不能为空'},{ type: 'number', message: '手机号必须为数字值'}]"
                     )
-                    el-input(type="text",v-model.number="form.mobeli",autocomplete="off")
+                    el-input(type="text",v-model.number="form.mobile",autocomplete="off")
                 el-form-item(
                     size='medium',
                     label="验证码",
                     prop="testCode"
                     )
                     el-input(type='text',v-model.trim="form.testCode",autocomplete="off",class='wth470')
-                    el-button(@click="getTestCode") 获取验证码
+                    el-button(@click="getTestCode('Form')") 获取验证码
 
                 el-form-item
                     el-button(type="primary",@click="registerHandle('Form')") 提交
                     el-button(@click="resetForm('Form')") 重置
+        el-dialog(title="完善信息",:visible.sync="dialogFormVisible")       
+            el-form(:model="userform",label-width='100px')     
+                el-form-item(label="用户名")
+                    el-input(v-model="form.username",autocomplete="off")
+                el-form-item(label="性别")
+                    el-radio(v-model="userform.sex" label="1") 男
+                    el-radio(v-model="userform.sex" label="0") 女
+                el-form-item(label="密码")
+                    el-input(type='password',v-model="form.password",autocomplete="off")
+            div(slot="footer")
+                el-button(type="primary" @click="dialogFormVisible = false") 确 定    
 </template>
 <script>
 export default {
@@ -28,25 +39,51 @@ export default {
         return{
             // 注册表单
             form:{
-                mobeli:'', //手机号
+                mobile:'', //手机号
                 testCode:'', // 验证码
-            }
+            },
+            userform:{// 用户信息表单
+                username:'',//用户名
+                sex:'',//性别
+                password:'',//密码
+            },
+            // 弹窗的显隐
+            dialogFormVisible:false,
         }
     },
     methods:{
         // 获取验证码
-        getTestCode(){
-
+        getTestCode(formName){
+            this.$refs[formName].validate((valid) => {
+                if (valid) {
+                    this.Axios({
+                        methods:'post',
+                        url:'/api/user/getTestCode',
+                        data:{
+                            mobile:this.mobile
+                        }
+                    }).then(data => {
+                        console.log(data)
+                    }).catch(err => {
+                        console.log(err)
+                    })
+                } else {
+                    console.log('error submit!!');
+                    return false;
+                }
+            });
+            
         },
         // 提交注册表单方法
         registerHandle(formName) {
             this.$refs[formName].validate((valid) => {
-            if (valid) {
-                alert('submit!');
-            } else {
-                console.log('error submit!!');
-                return false;
-            }
+                if (valid) {
+                    // alert('submit!');
+                    this.dialogFormVisible = true
+                } else {
+                    console.log('error submit!!');
+                    return false;
+                }
             });
         },
         resetForm(formName) {
@@ -81,7 +118,9 @@ export default {
             width:700px;
         }
     }
-    
+    .el-dialog{
+        text-align: left;
+    }
     .wth470{
         width:470px;
     }
--- chileme-cli/vue.config.js
+++ chileme-cli/vue.config.js
@@ -5,5 +5,25 @@ module.exports ={
           .use('pug-html-loader')
           .loader('pug-html-loader')
           .end()
-      }
+    },
+
+    devServer: {
+        proxy: {
+            '/api': {
+              target: 'http://127.0.0.1:3000',
+              ws: true,
+              changeOrigin: true,
+              pathRewrite:{
+                  '^/api':''
+              }
+            },
+            
+        }
+    },
+    // devServer:{
+    //     // 请求数据的服务器的地址
+    //     proxy:'http://127.0.0.1:3000'
+    // },
+
+    productionSourceMap: false
 }
