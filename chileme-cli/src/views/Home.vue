0fced957b52931c55ef3b65a49bf282cc53cf122
apondon <apondon@126.com>
2020-04-27 11:24:55 +0800

    左侧按钮样式

--- chileme-cli/src/views/Home.vue
+++ chileme-cli/src/views/Home.vue
@@ -3,6 +3,18 @@
       el-row
           el-col(:span='1')
               div.grid-left
+                  div.iconfont &#xe642;
+                    i 收银
+                  div.iconfont &#xe616;
+                    i 会员
+                  div.iconfont &#xe65f;
+                    i 库存
+                  div.iconfont &#xe64c;
+                    i 报表
+                  div.iconfont &#xe600;
+                    i 辅助
+                  div.iconfont &#xe650;
+                    i 退出
           el-col(:span='23')
               div.grid-right 
 </template>
@@ -25,7 +37,24 @@ export default {
     height: 100%;
   }
   .grid-left{
-    background: mediumblue;
+    display:flex;
+    flex-direction: column;
+    background: rgb(0, 188, 221);
+    color:#fff;
+    .iconfont{
+      flex:1;
+      display:flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      font-size: 40px;
+      i{
+        font-size: 16px;
+      }
+      &:hover{
+        background: rgb(235, 0, 47);
+      }
+    }
   }
 }
 
