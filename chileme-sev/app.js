917324f18a58c6f5a2714734133289650fe1a5d1
apondon <apondon@126.com>
2020-04-28 08:58:19 +0800

    安装配置koa-cors

--- chileme-sev/app.js
+++ chileme-sev/app.js
@@ -4,6 +4,8 @@ const cfg = require('./config')  //引入配置文件
 const mongoose = require('mongoose') //引入mongoose
 const router = require('./router/router')
 const koaBody = require('koa-body') // 引入koa-body插件解析post请求
+const koaCors = require('koa-cors') // 引入koa-cors插件
+
 
 // 创建服务器链接
 mongoose.connect(cfg.path)
@@ -14,6 +16,10 @@ mongoose.connection.once('open',()=>{
     console.log('数据库已链接')
 })
 App.use(koaBody()) //配置koa-body
+App.use(koaCors({
+    origin:'http://localhost:8080', //允许哪个地址来访问我们的服务
+    credentials:true, // 请求是否携带证书  cookie 
+})) //配置koa-cors
 App.use(router.routes()).use(router.allowedMethods())   /*启动路由*/
 
 // 启动服务器
